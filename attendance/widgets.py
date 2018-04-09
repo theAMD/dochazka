c4970b6bb265d9257a7378166a4a60c9d7f6301a
ReFuse <refuse@inuits.eu>
2018-04-09 20:48:19 +0200

    NextEventWidget fix

--- attendance/widgets.py
+++ attendance/widgets.py
@@ -15,10 +15,11 @@ class NextEventDashboardWidget(DashboardWidget):
         self.events = Event.objects.filter(calendar__unit__members__in=[self.user.person])\
             .filter(Q(end__gte=datetime.now()) | Q(start__lte=datetime.now(), end__gte=datetime.now()))\
             .order_by('end').distinct().all()
-        if self.events[0].start.date() == datetime.today().date():
-            self.firstEventIsToday = True
-        else:
-            self.firstEventIsToday = False
+        self.firstEventIsToday = False
+        if len(self.events) > 0:
+            if self.events[0].start.date() == datetime.today().date():
+                self.firstEventIsToday = True
+
 
     def render(self):
         return render(self.request, 'widgets/next_event_widget.html', {'events': self.events, 'firstEventIsToday': self.firstEventIsToday}).content
